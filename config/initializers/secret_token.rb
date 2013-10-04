178fb4486595471164e0b7beb472329ee5bf45e6
jeribo <jeribo@gmail.com>
2013-10-03 21:09:50 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveySupport::Application.config.secret_token = 'a37e9dafc48faa47df5613ca2cd89dba93537dc2a25710b95d336e3dc6dde4abbc4361853b3478fe01fad0843b198f657601141f4d1e340079f03a0b237986ec'
